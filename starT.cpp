3083bea6172526e6ce068a114d75f106d7866ffe
Geovane Marquez <geovane@cs.ucsb.edu>
2019-01-31 14:45:16 -0800

    updating 7th

--- starT.cpp
+++ starT.cpp
@@ -27,7 +27,10 @@ string starT(int width, int height)
   { 
 	for(int row=1; row<(width+1)/2;row++)
 		result+= " "; 
-	result +="* \n ";
+	result +="*";
+	for (int row=1; row<(width+1)/2;row++)
+		result+= " ";
+	result+="\n";
   }
   return result;
 }
