74581460664cfd13af1d6dc5cfecc6a7bce9e946
Geovane Marquez <geovane@cs.ucsb.edu>
2019-01-31 19:52:55 -0800

    More updates

--- starZ.cpp
+++ starZ.cpp
@@ -29,10 +29,12 @@ string starZ(int width)
   {
   	for (int col = 1; col <= row - 2; col++)
 		result += " ";
-	result += "* \n";
+	result += "*";
+	for (int col = 2; col <= row - 2; col++)
+		result += " ";
+	result += "\n";
 	
   }
-  result += " ";
   //bottom Z row
   for (int row = 1; row <= width; row++)
 	result += "*";
